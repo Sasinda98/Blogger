dd8539e8e302af73f7050d1cad64b2faec0bd9c6
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-25 12:29:07 +0530

    FIX FOR WARNING warning: Unmapped target properties: "comments, removeComment"

--- src/main/java/com/gsr/blogger/service/mapper/PostMapper.java
+++ src/main/java/com/gsr/blogger/service/mapper/PostMapper.java
@@ -2,6 +2,7 @@ package com.gsr.blogger.service.mapper;
 
 import com.gsr.blogger.domain.*;
 import com.gsr.blogger.service.dto.PostDTO;
+import java.util.List;
 import org.mapstruct.*;
 
 /**
@@ -13,4 +14,38 @@ public interface PostMapper extends EntityMapper<PostDTO, Post> {
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     PostDTO toDtoId(Post post);
+
+    @Override
+    @Mapping(target = "comments", ignore = true)
+    @Mapping(target = "removeComment", ignore = true)
+    Post toEntity(PostDTO dto);
+
+    @Override
+    default void partialUpdate(Post entity, PostDTO dto) {
+        if (dto == null) {
+            return;
+        }
+
+        if (dto.getId() != null) {
+            entity.id(dto.getId());
+        }
+        if (dto.getTitle() != null) {
+            entity.setTitle(dto.getTitle());
+        }
+        if (dto.getContent() != null) {
+            entity.setContent(dto.getContent());
+        }
+        if (dto.getLikes() != null) {
+            entity.setLikes(dto.getLikes());
+        }
+        if (dto.getCreatedAt() != null) {
+            entity.setCreatedAt(dto.getCreatedAt());
+        }
+        if (dto.getModifiedAt() != null) {
+            entity.setModifiedAt(dto.getModifiedAt());
+        }
+        if (dto.getIsDeleted() != null) {
+            entity.setIsDeleted(dto.getIsDeleted());
+        }
+    }
 }
